8174ff2893a923586ee8f7e1ebb8bcaf2374a9d9
Michael Babker <michael.babker@gmail.com>
2019-07-17 18:05:45 -0500

    No extended constructor

--- src/Exception/UnableToWriteBody.php
+++ src/Exception/UnableToWriteBody.php
@@ -15,16 +15,4 @@ namespace Joomla\Application\Exception;
  */
 class UnableToWriteBody extends \DomainException
 {
-	/**
-	 * Constructor.
-	 *
-	 * @param   string      $message   The Exception message to throw.
-	 * @param   int         $code      The Exception code.
-	 * @param   \Exception  $previous  The previous exception used for the exception chaining.
-	 *
-	 * @since   2.0.0
-	 */
-	public function __construct($message = '', $code = 500, \Exception $previous = null)
-	{
-	}
 }
