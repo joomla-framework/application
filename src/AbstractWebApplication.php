0a14507d5091eaa514ce94773780bc1bd6a785eb
Sven Schultschik <sven@schultschik.de>
2025-07-04 22:42:36 +0200

    fix cli scheduler:run breaks if --live-site does not end with / (#123)
    * fix cli scheduler:run breaks if --live-site does not end with /
    
    Signed-off-by: Sven Schultschik <sven@schultschik.de>
    
    * Use str_starts_with/str_ends_with instead
    
    * Reverting previous commit
    
    * Use str_starts_with and str_ends_with properly
    
    ---------
    
    Signed-off-by: Sven Schultschik <sven@schultschik.de>
    Co-authored-by: Hannes Papenberg <info@joomlager.de>

--- src/AbstractWebApplication.php
+++ src/AbstractWebApplication.php
@@ -817,7 +817,11 @@ abstract class AbstractWebApplication extends AbstractApplication implements Web
         } else {
             // If not in "Apache Mode" we will assume that we are in an IIS environment and proceed.
             // IIS uses the SCRIPT_NAME variable instead of a REQUEST_URI variable... thanks, MS
-            $uri .= $this->input->server->getString('SCRIPT_NAME');
+            $scriptname = $this->input->server->getString('SCRIPT_NAME');
+            if (!str_starts_with($scriptname, '/') && !str_ends_with($uri, '/')) {
+                $uri .= '/';
+            }
+            $uri .= $scriptname;
             $queryHost = $this->input->server->getString('QUERY_STRING', '');
 
             // If the QUERY_STRING variable exists append it to the URI string.
