c294a89a115d8edd6d053b95125814dd0d2ebb38
Hannes Papenberg <info@joomlager.de>
2025-07-04 18:18:00 +0200

    Reverting previous commit

--- src/AbstractWebApplication.php
+++ src/AbstractWebApplication.php
@@ -818,7 +818,7 @@ abstract class AbstractWebApplication extends AbstractApplication implements Web
             // If not in "Apache Mode" we will assume that we are in an IIS environment and proceed.
             // IIS uses the SCRIPT_NAME variable instead of a REQUEST_URI variable... thanks, MS
             $scriptname = $this->input->server->getString('SCRIPT_NAME');
-            if (!(str_starts_with($scriptname, '/') && str_ends_with($uri, '/'))) {
+            if (substr($scriptname, 0, 1) !== '/' && substr($uri, -1) !== '/') {
                 $uri .= '/';
             }
             $uri .= $scriptname;
